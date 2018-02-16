713a96a113a752cec8f9f51b967b4fc976cd2a17
Yoscroll <ianhenry.c808@gmail.com>
2018-02-16 12:55:15 -1000

    HTML~Redo & Add,CSS~May need to Fix, Java~Still Working

---
+++ generator.js
@@ -0,0 +1,22 @@
+/*Fortunes:
+1-   Doflamingo will make you shoot yourself.
+2-   Candy will make you disappear and will make you a slave without anyone knowing you completly.
+3-   Law cuts off your heads using his ability to feed the Sea Emperors.
+4-   Carrot will bite you to death.
+5-   Ceasar's laugh will annoy you to death.
+6-   Buggy's nose is so stupid, you die by laugther.
+7-   You laughed at Pica's laugh, which made you all but blood on the wall.
+8-   Akainu found you and executed you right at the spot.
+9-   You don't eat and you threw away Sanji's food, he barbecued you to feed you.
+10-  You pissed off Big Mama. She then steals your soul.
+11-  
+12-  
+13-  
+14-  
+15-  
+16-  
+17-  
+18-  
+19-  
+20-  
+*/
